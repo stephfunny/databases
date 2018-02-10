3e206a84eb465298b195e281c01619478c5057c1
stephfunny <contactstephanief@gmail.com>
2018-02-09 21:17:13 -0800

    working on get & post

--- server/models/index.js
+++ server/models/index.js
@@ -1,11 +1,33 @@
-var db = require('../db');
+var db = require('../db'); //get or post info using db.something
+var express = require('express');
+var mysql = require('mysql');
+
+
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
-  },
+    get: function (callback) {
+      console.log(mysql.query);
+      db();
 
+      // db.connection.query('SELECT * FROM messages', function(err, results, fields) {
+      //   if (err) {
+      //     console.log('error');
+      //     throw err;
+      //   } else {
+      //     console.log('THIS thy RESULT: ', results);
+      //     if (callback) { 
+      //       callback(results);
+      //     }
+      //   }
+      // });
+    }, // a function which produces all the messages
+    post: function () {
+
+    } 
+     
+    // a function which can be used to insert a message into the database
+  },
   users: {
     // Ditto as above.
     get: function () {},
@@ -13,3 +35,4 @@ module.exports = {
   }
 };
 
+module.exports.messages.get();
